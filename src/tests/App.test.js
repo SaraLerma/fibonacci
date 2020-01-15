f0facc49a7f52c608b3fd0c5e53787e4038ec0d2
SaraLerma <saralerma45@gmail.com>
2020-01-15 18:42:19 +0100

    Finish App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,13 +2,13 @@ import React from "react";
 import { render, fireEvent} from "@testing-library/react";
 import App from "../App";
 
-test("check is found title", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Fibonacci calculator/i);
-  expect(linkElement).toBeInTheDocument();
+test("displays application title", () => {
+  const { queryByText } = render(<App />); 
+  const title = queryByText(/Fibonacci calculator/i);
+  expect(title).toBeInTheDocument();
 });
 
-test("check is part of the description", () => {
+test("check final of the description", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/starting with 0 and 1/i);
   expect(linkElement).toBeInTheDocument();
@@ -37,4 +37,30 @@ test("not displays result when enter in the app", () => {
   const { queryByLabelText } = render(<App />); 
   const result = queryByLabelText(/result/i);
   expect(result).not.toBeInTheDocument();
+});
+
+test("when input is positive then display the fibonacci result", () => {
+  const { queryByLabelText } = render(<App />);
+
+  const input = queryByLabelText('fib-input')
+  fireEvent.change(input, { target: { value: '1' } })
+  
+  const button = queryByLabelText(/calculate/i);
+  fireEvent.click(button);  
+  
+  const result = queryByLabelText(/result/i);
+  expect(result).toBeInTheDocument();
+});
+
+test("when input a number higher than 1000 display error", () => {
+  const { queryByText, queryByLabelText } = render(<App />);
+
+  const input = queryByLabelText('fib-input')
+  fireEvent.change(input, { target: { value: '1001' } })
+  
+  const button = queryByLabelText(/calculate/i);
+  fireEvent.click(button);  
+  
+  const error = queryByText(/ERROR: Number must be positive between 0 and 1000/i)
+  expect(error).toBeInTheDocument()
 });
